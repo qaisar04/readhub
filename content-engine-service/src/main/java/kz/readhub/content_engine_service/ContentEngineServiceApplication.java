5c9525de5ce805c672c0cac629c049b6ccb3030f
Балтабаев Кайсар Бегисович <q.baltabayev@icloud.com>
2025-03-30 19:10:39 +0500

    initialize content engine service with basic structure and configuration

---
+++ content-engine-service/src/main/java/kz/readhub/content_engine_service/ContentEngineServiceApplication.java
@@ -0,0 +1,12 @@
+package kz.readhub.content_engine_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentEngineServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentEngineServiceApplication.class, args);
+    }
+}
